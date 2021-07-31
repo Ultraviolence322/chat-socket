70e13f2082dad341dc58369269309a15934fec40
unknown <segeichenkov18@gmail.com>
2021-07-31 09:50:39 +0300

    added button to clear messages

--- client/src/components/Chat/GeneralChat.jsx
+++ client/src/components/Chat/GeneralChat.jsx
@@ -1,12 +1,21 @@
-import React, {useState} from 'react'
+import React from 'react'
 import ChatWindow from './ChatWindow'
 import MessageForm from './MessageForm'
 
-export default function GeneralChat({userName, userId, socket}) {
-  const [messages, setMessages] = useState([])
+import { MESSAGES_KEY } from '../../constants/local-storage-keys';
+
+export default function GeneralChat({userName, userId, socket, messages, setMessages}) {
+
+  function clearMessages() {
+    localStorage.setItem(MESSAGES_KEY, JSON.stringify([]))
+    setMessages([])
+  }
 
   return (
     <div>
+      <div>
+        <button onClick={clearMessages}>Clear messages</button>
+      </div>
       <ChatWindow messages={messages} />
       <MessageForm  setMessages={setMessages} userName={userName} userId={userId} socket={socket}/>
     </div>
