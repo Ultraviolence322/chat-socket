8261b05440493278bf9494109d5468579dd7350c
Vladislav Segeichenkov <segeichenkov18@gmail.com>
2021-08-07 11:24:27 +0300

    added conversations list to GeneralChat

---
+++ client/src/components/Chat/Conversations.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Conversations() {
+  return (
+    <div>
+      <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+      </ul>
+    </div>
+  )
+}
--- client/src/components/Chat/GeneralChat.jsx
+++ client/src/components/Chat/GeneralChat.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ChatWindow from './ChatWindow'
 import MessageForm from './MessageForm'
+import Conversations from './Conversations'
 
 import { MESSAGES_KEY } from '../../constants/local-storage-keys';
 
-export default function GeneralChat({userName, userId, socket, messages, setMessages, roomId}) {
+export default function GeneralChat({userName, userId, socket, messages, setMessages, roomId, handleCloseChat}) {
 
   function clearMessages() {
     localStorage.setItem(MESSAGES_KEY, JSON.stringify([]))
@@ -12,12 +13,20 @@ export default function GeneralChat({userName, userId, socket, messages, setMess
   }
 
   return (
-    <div>
-      <div>
-        <button onClick={clearMessages}>Clear messages</button>
+    <div className="general-chat">
+      <div className="conversations">
+        <Conversations />
       </div>
-      <ChatWindow messages={messages} />
-      <MessageForm roomId={roomId} setMessages={setMessages} userName={userName} userId={userId} socket={socket}/>
+      <div>
+        <div>
+          <button onClick={clearMessages}>Clear messages</button>
+        </div>
+        <ChatWindow messages={messages} />
+        <MessageForm roomId={roomId} setMessages={setMessages} userName={userName} userId={userId} socket={socket}/>
+        <button onClick={handleCloseChat}>Close chat</button>
+
+      </div>
+      
     </div>
   )
 }
